cc972a6fadf79bcd16546e68e8db69b8eb0af7ba
Nancy Zaky <nancyzki@yahoo.com>
2021-07-11 22:48:39 -0700

    Assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let result = e.target.children[1].value;
+    let toDoList = document.getElementById("tasks");
+    let item = document.createElement("li");
+    item.innerText = result;
+    toDoList.appendChild(item);
+  });
+  //let buttonClick = document.getElementById("");
 });
